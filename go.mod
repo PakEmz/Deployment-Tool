39635ccd16464b7ca72c71f376ede1187d76a711
jobin <jobinjose01@gmail.com>
2021-10-16 12:28:39 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module windowtest
+
+go 1.17
+
+require (
+	fyne.io/fyne/v2 v2.1.0
+	github.com/go-gl/gl v0.0.0-20210905235341-f7a045908259 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+
+	os.Setenv("FYNE_THEME", "light")
+
+	a := app.New()
+	win := a.NewWindow("Deployment Tool")
+	win.Resize(fyne.NewSize(600, 300))
+	win.CenterOnScreen()
+	username := widget.NewEntry()
+	username.SetPlaceHolder("User Name of the Server...")
+	ipaddress := widget.NewEntry()
+	ipaddress.SetPlaceHolder("IP Address of the Server...")
+	textArea := widget.NewMultiLineEntry()
+	textArea.Resize(fyne.NewSize(100, 200))
+	cancelbutton := widget.NewButton("CLOSE", func() {
+
+		os.Exit(0)
+	})
+
+	deploybutton := widget.NewButton("DEPLOY", func() {
+		usrname := username.Text
+		ipaddr := ipaddress.Text
+		if usrname != "" && ipaddr != "" {
+			textArea.SetText("The Deployment is in Progress ....")
+			result := Executeshell(usrname, ipaddr)
+			textArea.SetText(result)
+		} else {
+			textArea.SetText("Please fill the details!")
+		}
+	})
+
+	vlayout := container.New(layout.NewVBoxLayout(),
+		username,
+		ipaddress,
+		container.New(layout.NewGridLayout(2), deploybutton, cancelbutton),
+		//textGrid,
+	)
+	win.SetContent(container.New(layout.NewGridLayout(2), vlayout, textArea))
+
+	win.ShowAndRun()
+}
+
+func Executeshell(usrname string, ipaddr string) string {
+
+	script := `
+
+	cd /home/admin/web/codfoxx.in/public_html/
+
+	ls
+
+	cat index.html
+
+	`
+
+	c := exec.Command("ssh", usrname+"@"+ipaddr, "-i", "codfoxxsshaccess.pem")
+	var buf = new(bytes.Buffer)
+	buf.WriteString(script)
+	c.Stdin = buf
+
+	b, e := c.Output()
+	if e != nil {
+		fmt.Println(e)
+	}
+	return (string(b))
+
+}
